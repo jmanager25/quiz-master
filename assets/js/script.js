1fb70c67e75a208ba7ae4387409b08c872fac701
jmanager25 <jmanager25@hotmail.com>
2022-12-03 08:20:34 +0000

    Declare the categories variables

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+// Categories
+const history = document.getElementById("history");
+const sports = document.getElementById("sports");
+const science = document.getElementById("science");
+const musicCinema = document.getElementById("music-cinema");
+const geography = document.getElementById("geography");
+const artLiterature = document.getElementById("art-literatue");
