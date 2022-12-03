0ef91be8d206ae5e2a1e9b2d658c5971b1c64285
jmanager25 <jmanager25@hotmail.com>
2022-12-03 08:48:20 +0000

    Declare landing page and play area variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+// Variables 
+// Landing page
+const rules = document.getElementById("rules");
+const play = document.getElementById("play");
+const hiscore = document.getElementById("hiscore");
 
 // Categories
 const history = document.getElementById("history");
@@ -6,3 +11,14 @@ const science = document.getElementById("science");
 const musicCinema = document.getElementById("music-cinema");
 const geography = document.getElementById("geography");
 const artLiterature = document.getElementById("art-literatue");
+
+// Play area
+const soundOn = document.getElementById("sound-on");
+const SoundOff = document.getElementById("sound-off");
+let timer = document.getElementById("timer");
+const question = document.getElementById("question");
+const answerA = document.getElementById("answerA");
+const answerB = document.getElementById("answerB");
+const answerC = document.getElementById("answerC");
+const answerD = document.getElementById("answerD");
+const nextQuestion = document.getElementById("next-question");
